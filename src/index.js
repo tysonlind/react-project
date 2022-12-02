8abd3e66e4c94743fab8d36a14d7611ec21c0d2f
tyson lind <tysonfloydlind@gmail.com>
2022-12-02 11:05:50 -0700

    deleted unused import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FilmsList from './components/filmsList.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
